7d8151ce794636760c977e3367135fe4e58ea369
Gibson Berglund <berglundgibson@gmail.com>
2022-11-08 15:02:25 -0600

    added a bunch of prompts and messy for loops

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,58 @@
 // Assignment code here
 
+var pwleng = prompt("How many characters do you want your password to be?");
+  if (pwleng === "8") {
+    var pwcase = prompt("Case sensitive?");
+      if (pwcase === "y"){
+        var pwcasenum = prompt("Include numbers?");
+          if (pwcasenum === "y") {
+            var pwcasenumchar = prompt("Include special characters?");
+              if (pwcasenumchar === "y") {
+                prompt("cool");
+              } else if (pwcasenumchar === "n") {
+                prompt("aight");
+              } else {
+                prompt("Sorry that is not a valid input. Please choose y or n.")
+              } 
+          } else if (pwcasenum === "n") {
+            var pwcasenonumchar = prompt("Include special characters?")
+              if (pwcasenonumchar === "y") {
+                prompt("word");
+              } else if (pwcasenunumchar === "n") {
+                prompt("gotcha");
+              } else {
+                prompt("Sorry");
+              }
+          }
+        } else if (pwcase === "n") {
+          var psnocasenum = ("Include numbers?");
+            if (psnocasenum === "y") {
+              var psnocasenumchar = prompt("Include special characters?");
+                if (psnocasenumchar === "y") {
+                  prompt("noice");
+                } else if (psnocasenumchar === "n") {
+                  prompt("k");
+                } else {
+                  prompt("Sorry");
+                }
+            } else if (psnocasenum === "n") {
+              var psnocasenonumchar = prompt("Include special characters?");
+                if (psnocasenonumchar === "y") {
+                  prompt("right");
+                } else if (psnocasenonumchar === "n") {
+                  prompt("okay");
+                } else {
+                  prompt("Sorry");
+                }
+            } else {
+              prompt("sorry");
+              }
+        } else{
+            prompt("Sorry that is not a valid option. Choose y or n.");
+          }
+     } else {
+    prompt("Sorry that is not a valid option. Choose a number between 8 and 128.");
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
