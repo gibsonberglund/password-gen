3b52cee204c5245420880f9ddbbb0bda053ef290
Gibson Berglund <berglundgibson@gmail.com>
2022-11-08 17:12:03 -0600

    nested for loops inside conditionals, added random generator, defined character sets

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,117 @@
 // Assignment code here
 
-var pwleng = prompt("How many characters do you want your password to be?");
-  if (pwleng === "8") {
+//declare all character sets
+var charsnumspec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+var charsnonumspec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()';
+var charsnonumnospec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var charsnumnospec = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var charsnocasenumnospec = 'abcdefghijklmnopqrstuvwxyz0123456789';
+var charsnocasenonumnospec = 'abcdefghijklmnopqrstuvwxyz';
+var charsnocasenonumspec = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()';
+
+//create array that will contain password characters
+var pwleng = [];
+
+//ask for specifications from user
+pwleng.length = prompt("How many characters do you want your password to be?");
+//chekcs valid entry for character number
+  if (pwleng.length < 8){
+    console.log("sorry");
+  } else if (pwleng.length>128){
+    console.log("sorry");
+//asks for other specifications
+  } else {
     var pwcase = prompt("Case sensitive?");
       if (pwcase === "y"){
         var pwcasenum = prompt("Include numbers?");
           if (pwcasenum === "y") {
             var pwcasenumchar = prompt("Include special characters?");
               if (pwcasenumchar === "y") {
-                prompt("cool");
+         //prints result for all yes answers
+                for (var i=0; i < pwleng.length; i++) {
+                  pwleng[i] = charsnumspec.charAt(Math.floor(Math.random() * charsnumspec.length));
+                }
+                  console.log(pwleng.toString());
               } else if (pwcasenumchar === "n") {
-                prompt("aight");
-              } else {
+         //prints result for yes to numbers and case sensitive but no to special characters
+                for (var i=0; i < pwleng.length; i++) {
+                  pwleng[i] = charsnumnospec.charAt(Math.floor(Math.random() * charsnumnospec.length));
+                }
+                  console.log(pwleng.toString());
+          //invalid input
+                } else {
                 prompt("Sorry that is not a valid input. Please choose y or n.")
               } 
           } else if (pwcasenum === "n") {
             var pwcasenonumchar = prompt("Include special characters?")
               if (pwcasenonumchar === "y") {
-                prompt("word");
-              } else if (pwcasenunumchar === "n") {
-                prompt("gotcha");
-              } else {
+          // prints result for no to numbers but yes to case sensitive and special characters
+                for (var i=0; i < pwleng.length; i++) {
+                  pwleng[i] = charsnonumspec.charAt(Math.floor(Math.random() * charsnonumspec.length));
+                }
+                  console.log(pwleng.toString());
+              } else if (pwcasenonumchar === "n") {
+          //prints result for no number, yes case sensitive, no special characters
+                for (var i=0; i < pwleng.length; i++) {
+                  pwleng[i] = charsnonumnospec.charAt(Math.floor(Math.random() * charsnonumnospec.length));
+                }
+                  console.log(pwleng.toString());
+          //invalid input 
+                } else {
                 prompt("Sorry");
               }
           }
         } else if (pwcase === "n") {
-          var psnocasenum = ("Include numbers?");
+          var psnocasenum = prompt("Include numbers?");
             if (psnocasenum === "y") {
               var psnocasenumchar = prompt("Include special characters?");
                 if (psnocasenumchar === "y") {
-                  prompt("noice");
+          //prints result for not case sensitive, yes numbers and special characters
+                  for (var i=0; i < pwleng.length; i++) {
+                    pwleng[i] = charsnocasenumspec.charAt(Math.floor(Math.random() * charsnocasenumspec.length));
+                  }
+                    console.log(pwleng.toString());
                 } else if (psnocasenumchar === "n") {
-                  prompt("k");
-                } else {
+          //prints result for not case sensitive, yes numbers, no special characters
+                  for (var i=0; i < pwleng.length; i++) {
+                    pwleng[i] = charsnocasenumnospec.charAt(Math.floor(Math.random() * charsnocasenumnospec.length));
+                  }
+                    console.log(pwleng.toString());
+          //invalid input
+                  } else {
                   prompt("Sorry");
                 }
             } else if (psnocasenum === "n") {
               var psnocasenonumchar = prompt("Include special characters?");
                 if (psnocasenonumchar === "y") {
-                  prompt("right");
+          //prints result for not case sentitive, no numbers, yes to special characters
+                  for (var i=0; i < pwleng.length; i++) {
+                    pwleng[i] = charsnocasenonumspec.charAt(Math.floor(Math.random() * charsnocasenonumspec.length));
+                  }
+                    console.log(pwleng.toString());
                 } else if (psnocasenonumchar === "n") {
-                  prompt("okay");
+          //prints result for not case sentitive, no numbers, and no special characters
+                  for (var i=0; i < pwleng.length; i++) {
+                    pwleng[i] = charsnocasenonumnospec.charAt(Math.floor(Math.random() * charsnocasenonumnospec.length));
+                  }
+                    console.log(pwleng.toString());
                 } else {
+          //invalid input
                   prompt("Sorry");
                 }
             } else {
+          //invalid input
               prompt("sorry");
               }
         } else{
+          //invalid input
             prompt("Sorry that is not a valid option. Choose y or n.");
           }
-     } else {
-    prompt("Sorry that is not a valid option. Choose a number between 8 and 128.");
   }
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
